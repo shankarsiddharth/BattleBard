5fdc5980e03fa17de91196b3f7a7739a9155668a
Ogulcan Buyuksandalyaci <ogulcan.bs96@gmail.com>
2022-02-14 14:06:23 -0700

    GameEvents class implemented

---
+++ BattleBard/Assets/Code/GameEvents.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #region Minion Events
+    //Change types from int to Minion when class is implemented
+    public event Action<int, int> onMinionAttackStarted;
+    public event Action<int> onMinionDeath;
+
+    public void MinionAttackStarted(int attackerId, int otherId)
+    {
+        onMinionAttackStarted(attackerId, otherId);
+    }
+
+    public void MinionDeath(int minionId)
+    {
+        onMinionDeath(minionId);
+    }
+    #endregion
+
+    #region Effect Events
+
+    //Change types after dependant classes are implemented
+    public event Action onAreaEffectApplied;
+    public event Action onEffectApplied;
+    public event Action onEffectExpired;
+
+    public void AreaEffectApplied()
+    {
+        onAreaEffectApplied();
+    }
+
+    public void EffectApplied()
+    {
+        onEffectApplied();
+    }
+
+    public void EffectExpired()
+    {
+        onEffectExpired();
+    }
+    #endregion
+
+    #region Drum Events
+    //Maybe change drum id to enum again after drum class implementation?
+    public event Action<int> onDrumPlayed;
+
+    //Add parameters after implementations of effects and stuff
+    public event Action onDrumComboCompleted;
+
+    public void DrumPlayed(int drumId)
+    {
+        onDrumPlayed(drumId);
+    }
+    
+    public void DrumComboCompleted()
+    {
+        onDrumComboCompleted();
+    }
+    #endregion
+
+    #region Lane Events
+    public event Action<int> onUnitSpawnedInLane;
+
+    public void UnitSpawnedInLane(int waveCount)
+    {
+        onUnitSpawnedInLane(waveCount);
+    }
+    #endregion
+}
