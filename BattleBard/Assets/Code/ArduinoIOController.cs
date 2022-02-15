da340951a85871e80c7b10989c41adfa0ad911a6
tim trimble <u1005391@ad.utah.edu>
2022-02-15 08:33:07 -0700

    Reading input from controller and converting to int

--- BattleBard/Assets/Code/ArduinoIOController.cs
+++ BattleBard/Assets/Code/ArduinoIOController.cs
@@ -48,6 +48,28 @@ public class ArduinoIOController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ArduinoSerialPort.IsOpen)
+        {
+            try
+            {
+                string readData = ArduinoSerialPort.ReadLine();
+                if (string.IsNullOrEmpty(readData))
+                { 
+                    return;
+                }
+
+                Debug.Log("Recieved the following message from controller: " + readData);
+                int buttonPressed;
+                if(!int.TryParse(readData, out buttonPressed))
+                {
+                    Debug.Log("Unable to parse controller input to a number");
+                }
+            }
+            catch (System.Exception exception)
+            {
+                if(exception.Message != "The operation has timed out.")
+                    Debug.Log("Caught the following exception while trying to read input: " + exception.Message);
+            }
+        }
     }
 }
