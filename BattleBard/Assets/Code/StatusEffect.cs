33feb72380b3e7469ba00fa4e3428552c4c94030
KBHeid <kbheid@live.com>
2022-03-15 14:02:45 -0600

    VFX

--- BattleBard/Assets/Code/StatusEffect.cs
+++ BattleBard/Assets/Code/StatusEffect.cs
@@ -9,6 +9,8 @@ public class StatusEffect : ScriptableObject
 	public float[] tierDuration;
 	[Tooltip("The effects (applied multiplicitively) on the Stats of a minion. >1 for increase, <1 for decrease.\n Each item in the list represents a tier of the effect.")]
 	public Stats[] multiplicitiveChanges;
+	[Tooltip("The prefab of the effect to be applied as a child to the minions.")]
+	public GameObject[] effectVFX;
 
 	// Potentially takes GameObject instead of BaseAI and finds the PlayerBaseAI or EnemyBaseAI components
 	public IEnumerator StartTimer(int tier, BaseAI ai)
@@ -17,6 +19,8 @@ public class StatusEffect : ScriptableObject
 
 		if (!ai.stats.currentEffects.Contains(this))
 		{
+			// Add VFX
+			GameObject vfx = Instantiate(effectVFX[tier], ai.transform);
 			ai.stats.currentEffects.Add(this);
 			ai.stats *= multiplicitiveChanges[tier];
 
@@ -26,6 +30,7 @@ public class StatusEffect : ScriptableObject
 			{
 				ai.stats.currentEffects.Remove(this);
 				ai.stats /= multiplicitiveChanges[tier];
+				Destroy(vfx);
 			}
 
 			Debug.Log("Done.");
