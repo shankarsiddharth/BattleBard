c40fc3593c329b3a1202d9cd34a4fabeec7bf62b
Shankar Siddharth <7028991+shankarsiddharth@users.noreply.github.com>
2022-03-31 14:11:01 -0600

    Audio State

---
+++ BattleBard/Assets/Code/Combo/ComboEffects/DamageCombo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCombo : ComboApplyStatus
+{
+    void Start()
+    {
+        print("ATTACK!!");
+    }
+}
--- BattleBard/Assets/Code/ComboAudioWwise.cs
+++ BattleBard/Assets/Code/ComboAudioWwise.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 
 public class ComboAudioWwise : MonoBehaviour
 {
-    public AK.Wwise.Event comboAudioEvent;
-    public AK.Wwise.State akState;
+    public int defaultTierLevel = 0;
+    public int currentTierLevel = 0;
+    public int currentComboType = 0;
+    public List<AK.Wwise.Event> comboTierAudioEvent;
+    public List<AK.Wwise.State> akIdleState;
+    public List<AK.Wwise.State> akAttackState;
+    public List<AK.Wwise.State> akHealState;
+    public List<AK.Wwise.State> akSpeedState;
 
     void Awake()
     {
@@ -15,17 +21,27 @@ public class ComboAudioWwise : MonoBehaviour
 
     public void ListenForCombos(ComboBase effect, int level, Vector3 position)
     {
+        int tierLevel = level;
+        currentTierLevel = tierLevel;
         if(effect is SpeedCombo)
         {
-            //AkSoundEngine.SetState("Tier1", "Attacking");
-            akState.SetValue();
+            akSpeedState[tierLevel].SetValue();
+        }
+        else if(effect is DamageCombo)
+        {
+            akAttackState[tierLevel].SetValue();
+        }
+        else if (effect is HealingCombo)
+        {
+            akHealState[tierLevel].SetValue();
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        comboAudioEvent.Post(gameObject);
+        comboTierAudioEvent[currentTierLevel].Post(gameObject);
+        akHealState[currentTierLevel].SetValue();
         //AkSoundEngine.SetState("Tier1", "Attacking");
     }
 
